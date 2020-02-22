72451fa2e6606cebe89fa0a5cf9350527c0f56df
Jane Sonina <janeridebmx@gmail.com>
2020-02-22 20:40:34 +0300

    Fix validator

--- src/main/java/validator/TextComponentValidator.java
+++ src/main/java/validator/TextComponentValidator.java
@@ -4,8 +4,10 @@ import entity.ComponentType;
 import entity.Component;
 
 public class TextComponentValidator {
+    private TextComponentValidator() {
+    }
 
-    public static boolean isText(Component component){
-        return component !=null && component.getComponentType() == ComponentType.TEXT;
+    public static boolean isText(Component component) {
+        return component != null && component.getComponentType() == ComponentType.TEXT;
     }
 }
