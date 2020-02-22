643336d05179d4e276d711cce0929fb05cefd0ef
Jane Sonina <janeridebmx@gmail.com>
2020-02-22 18:33:13 +0300

    Add sevice

--- src/main/java/service/TextSorter.java
+++ src/main/java/service/TextSorter.java
@@ -1,32 +1,23 @@
 package service;
 
 import entity.TextComponent;
+import entity.TextComposite;
+import exception.SortException;
+
+import java.util.Collections;
+import java.util.Comparator;
 
 import static validator.TextComponentValidator.*;
 
 public class TextSorter {
-
-
-//    public void sortParagraphsByCountOfSentences() {
-//        Arrays.sort(paragraphs, comparingInt(o -> o.split(SENTENCE_SPLIT_REGEX).length));
-//    }
-
-    public void sortWordsInSentencesByLength() {
-        for (String[] sen : sentences)
-            Arrays.sort(sen, comparingInt(String::length));
-    }
-
-    public void sortWordsByFrequencyOfChar(char c) {
-        for (String[] sen : sentences)
-            Arrays.sort(sen, comparingInt((String o) -> countChar(o, c))
-                    .reversed()
-                    .thenComparing(String.CASE_INSENSITIVE_ORDER));
-    }
-
-    public void sortParagraphsByCountOfSentences(TextComponent textComponent){
-        if (isText(textComponent)){
-
+    public void sortParagraphsByCountOfSentences(TextComposite textComposite) throws SortException {
+        if (isText(textComposite)) {
+            Collections.sort(textComposite.getComponents(), (o1, o2) -> {
+                Comparator<TextComponent> comparator = Comparator.comparing(TextComponent::getCountComponents);
+                return comparator.compare(o1, o2);
+            });
         }
-
+        throw new SortException("Invalid type of data.");
+        //TODO log
     }
 }
